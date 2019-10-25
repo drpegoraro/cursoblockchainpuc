d6f238683a62d4586794c7964bbb2efcaed29c07
drpegoraro <56133734+drpegoraro@users.noreply.github.com>
2019-10-25 17:57:51 -0300

    Create relatoriohoras.sol

---
+++ relatoriohoras.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.5.12;
+
+contract relatorioDeHorasTrabalhadas
+
+{
+    string mes;
+    uint256 dia1;
+    uint256 dia2;
+    uint256 dia3;
+    uint256 dia4;
+    uint256 dia5;
+    uint256 dia6;
+    uint256 dia7;
+    uint256 dia8;
+    uint256 dia9;
+    uint256 dia10;
+    uint256 dia11;
+    uint256 dia12;
+    uint256 dia13;
+    uint256 dia14;
+    uint256 dia15;
+    uint256 dia16;
+    uint256 dia17;
+    uint256 dia18;
+    uint256 dia19;
+    uint256 dia20;
+    uint256 dia21;
+    uint256 dia22;
+    uint256 dia23;
+    uint256 dia24;
+    uint256 dia25;
+    uint256 dia26;
+    uint256 dia27;
+    uint256 dia28;
+    uint256 dia29;
+    uint256 dia30;
+    uint256 dia31;
+}
